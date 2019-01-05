5d6c8909b75746443164d7373fcc5e2f2436c535
Aleksey Malashenkov <malashenkov@protonmail.com>
2019-01-05 18:35:30 +0300

    Data Types

---
+++ TypesTest-Lesson-6/TypesTest-Lesson-6/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TypesTest-Lesson-6
+//
+//  Created by Алексей Малашенков on 05/01/2019.
+//  Copyright © 2019 Алексей Малашенков. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
