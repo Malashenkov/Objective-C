df9e9dfccca7522ead0108c83a546382ff2e5fdc
Aleksey Malashenkov <malashenkov@protonmail.com>
2019-01-08 12:56:47 +0300

    Add lesson 7

---
+++ ProtocolTest-Lesson-7/ProtocolTest-Lesson-7/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProtocolTest-Lesson-7
+//
+//  Created by Алексей Малашенков on 08/01/2019.
+//  Copyright © 2019 Алексей Малашенков. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProtocolTest-Lesson-7/ProtocolTest-Lesson-7/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  ProtocolTest-Lesson-7
+//
+//  Created by Алексей Малашенков on 08/01/2019.
+//  Copyright © 2019 Алексей Малашенков. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
