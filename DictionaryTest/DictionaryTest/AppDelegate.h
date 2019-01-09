270aa2d74c1bd0a7237d36ef2089a34f256a473d
Aleksey Malashenkov <malashenkov@protonmail.com>
2019-01-09 18:09:35 +0300

    Lesson 8

---
+++ DictionaryTest/DictionaryTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DictionaryTest
+//
+//  Created by Алексей Малашенков on 09/01/2019.
+//  Copyright © 2019 Алексей Малашенков. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DictionaryTest/DictionaryTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DictionaryTest
+//
+//  Created by Алексей Малашенков on 09/01/2019.
+//  Copyright © 2019 Алексей Малашенков. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
