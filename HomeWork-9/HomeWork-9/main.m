724eb41bb51ef63bf02b2b4fe71a65c2f657d937
Aleksey Malashenkov <malashenkov@protonmail.com>
2019-01-10 17:17:21 +0300

    HomeWork 9

---
+++ HomeWork-9/HomeWork-9/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HomeWork-9
+//
+//  Created by Алексей Малашенков on 10/01/2019.
+//  Copyright © 2019 Алексей Малашенков. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
