a209570d6d8e2bbdd28c1eee4c31769e91408ce6
Aleksey Malashenkov <malashenkov@protonmail.com>
2019-01-10 13:39:50 +0300

    Lesson 9

---
+++ DeligatesTest/DeligatesTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeligatesTest
+//
+//  Created by Алексей Малашенков on 10/01/2019.
+//  Copyright © 2019 Алексей Малашенков. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
