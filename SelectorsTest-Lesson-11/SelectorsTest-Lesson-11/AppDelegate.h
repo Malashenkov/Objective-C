5017bc7da120ab1dbc8b827c9f24139b05478dcd
Aleksey Malashenkov <malashenkov@protonmail.com>
2019-01-17 12:09:17 +0300

    Add lesson 11

---
+++ SelectorsTest-Lesson-11/SelectorsTest-Lesson-11/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SelectorsTest-Lesson-11
+//
+//  Created by Алексей Малашенков on 17/01/2019.
+//  Copyright © 2019 Алексей Малашенков. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SelectorsTest-Lesson-11/SelectorsTest-Lesson-11/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SelectorsTest-Lesson-11
+//
+//  Created by Алексей Малашенков on 17/01/2019.
+//  Copyright © 2019 Алексей Малашенков. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
