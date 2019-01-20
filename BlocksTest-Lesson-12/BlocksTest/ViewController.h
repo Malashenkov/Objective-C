957ad1bb8cef47a5b0ba159cebce91c251f951c7
Aleksey Malashenkov <malashenkov@protonmail.com>
2019-01-20 16:57:36 +0300

    Add lesson 12

---
+++ BlocksTest-Lesson-12/BlocksTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocksTest
+//
+//  Created by Алексей Малашенков on 20/01/2019.
+//  Copyright © 2019 Алексей Малашенков. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
